a6a2ef6008d4306c856cdfe7e172eff3d4ec7369
Tatjana Tsornei <tanjatsornei@hotmail.com>
2022-05-19 00:41:51 +0300

    add my seven2 c# project

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ANDOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AND - &&
+            //true && => true - username && password => welcome
+            //true && => false => False - username && password => acces denided
+            //false && true => false - username && password => access denided
+            //false && false => false - username && password 1234
+            // контрольные данные : username admin, password 1234
+            //Задание1. Дополнить приложение, чтобы дать пользователю только 3 попытки для входа в систему
+
+            int tries = 0;
+            while (tries != 3)
+            {
+                string username, password;
+                Console.WriteLine("Enter username:");
+                username = Console.ReadLine();
+
+                Console.WriteLine("Enter password:");
+                password = Console.ReadLine();
+
+                if (username == "admin" && password == "1234")
+                {
+                    Console.WriteLine("Welkome!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Access denided.");
+                    tries++;
+                    Console.WriteLine($"You have made {tries} attempt.");
+                }
+            }
+            Console.WriteLine("Have a nice day!");
+        }
+        
+    }
+}
